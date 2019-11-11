c092d958b205c599b2a34f7728dadb27c8ef9d56
Linsay McMahon <McMahon8900@gmail.com>
2019-11-11 03:20:12 -0600

    created alert questions

---
+++ script.js
@@ -0,0 +1,11 @@
+//Password alert quetions
+let passwordLength = prompt("Please indicate how long of a password you prefer", "Between 8 - 128 characters");
+    console.log(passwordLength);
+
+let specailCharacter = prompt("Special Characters?", "ex. !@#$%^&*()");
+
+let numericCharacter = prompt("Numbers");
+
+let lowerCase = prompt("Lowercase Characters?");
+
+let upperCase = prompt("Uppercase Characters?");
