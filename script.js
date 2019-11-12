056ad9b5592d3c5ec5500bbb7d6f6e7f6a9c5caa
Linsay McMahon <McMahon8900@gmail.com>
2019-11-12 16:47:02 -0600

    replaced alerts with getElementById and checked console.log of random generagtors

--- script.js
+++ script.js
@@ -1,11 +1,37 @@
-//Password alert quetions
-let passwordLength = prompt("Please indicate how long of a password you prefer", "Between 8 - 128 characters");
-    console.log(passwordLength);
+var char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+var num = "1234567890";
+var sym = "!@#$%^&*";
 
-let specailCharacter = prompt("Special Characters?", "ex. !@#$%^&*()");
+var charNum = document.getElementById("charNum");
+var numBox = document.getElementById("num");
+var symBox = document.getElementById("sym");
+var generatePassword = document.getElementById("generate password");
+var yourPassword = document.getElementById("your password");
 
-let numericCharacter = prompt("Numbers");
 
-let lowerCase = prompt("Lowercase Characters?");
+//console log works to generate random letters
+console.log(password(12, char));
 
-let upperCase = prompt("Uppercase Characters?");
+//console log works to generate random numbers
+console.log(password(12,num));
+
+//console log works to generate random symbols
+console.log(password(12,sym));
+
+// cannot get the code correct for it to display
+
+generatePassword.addEventListener("click", function(e){
+   var characters = char;
+   (numBox.checked) ? characters +- num: '';
+   (symBox.checked) ? characters +- sym : '';
+   yourPassword.value = password(charNum.value, characters);
+});
+
+
+function password(j, characters) {
+    var pwd = '';
+    for (var i = 0; i<j; i++) {
+        pwd += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return pwd;
+}
